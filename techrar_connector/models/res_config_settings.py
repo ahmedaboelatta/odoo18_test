8a71317c7ddeeb1404541c0cde8a7e06dc5cc734
Ahmed Abo EL-Atta <a7medaboelatta@gmail.com>
2026-07-15 23:21:12 +0300

    [update]

--- techrar_connector/models/res_config_settings.py
+++ techrar_connector/models/res_config_settings.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    techrar_api_base_url = fields.Char(
+    techrar_api_url = fields.Char(
         string='Techrar API Base URL',
         config_parameter='techrar.api_base_url',
         default='https://api.techrar.com',
