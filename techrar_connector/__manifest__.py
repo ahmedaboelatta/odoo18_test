a559b112a1f456ab673ef32d80266c85de063971
Ahmed Abo EL-Atta <a7medaboelatta@gmail.com>
2026-09-02 10:39:27 +0300

    u

--- techrar_connector/__manifest__.py
+++ techrar_connector/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Techrar Connector',
-    'version': '18.0.4.9.3',
+    'version': '18.0.4.9.4',
     'category': 'Integration',
     'summary': 'Techrar Platform API Integration for Odoo 18',
     'author': 'Ahmed Abo EL-Atta',
