8bd054e4ed592ed01d9655656e2dedba9c6af822
Ahmed Abo EL-Atta <a7medaboelatta@gmail.com>
2026-09-03 16:16:28 +0300

    u

--- invoice_company_letterhead/__manifest__.py
+++ invoice_company_letterhead/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Company Letterhead Reports',
-    'version': '18.0.7.8.1',
+    'version': '18.0.7.8.2',
     'summary': 'One company letterhead for sales, delivery, invoice, purchase and payment vouchers',
     'category': 'Accounting/Accounting',
     'author': 'Custom',
