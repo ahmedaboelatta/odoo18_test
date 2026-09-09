e4c5d067133aee9df9875a5080b0c092f51c9864
Ahmed Abo EL-Atta <a7medaboelatta@gmail.com>
2026-09-09 12:27:34 +0300

    u

--- account_restrict_journal/models/sale_order.py
+++ account_restrict_journal/models/sale_order.py
@@ -1,9 +1,36 @@
-from odoo import _, api, models
+from odoo import _, api, fields, models
 from odoo.exceptions import AccessError
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    available_invoicing_journal_ids = fields.Many2many(
+        comodel_name="account.journal",
+        compute="_compute_available_invoicing_journal_ids",
+        string="Available Invoicing Journals",
+    )
+
+    @api.depends("company_id")
+    @api.depends_context("uid", "allowed_company_ids")
+    def _compute_available_invoicing_journal_ids(self):
+        restricted = self.env.user.has_group(
+            "account_restrict_journal.account_restrict_journal_group_admin"
+        )
+        allowed_journal_ids = set(self.env.user.allowed_journal_ids.ids)
+        Journal = self.env["account.journal"].sudo()
+        for order in self:
+            journals = Journal.search(
+                [
+                    ("type", "=", "sale"),
+                    ("company_id", "=", order.company_id.id),
+                ]
+            )
+            order.available_invoicing_journal_ids = (
+                journals.filtered(lambda journal: journal.id in allowed_journal_ids)
+                if restricted
+                else journals
+            )
 
     @api.model
     def _check_allowed_invoicing_journal(self, journal):
@@ -36,8 +63,15 @@ class SaleOrder(models.Model):
             )
         return super().write(vals)
 
+    @api.onchange("company_id")
+    def _onchange_company_allowed_invoicing_journal(self):
+        if (
+            self.journal_id
+            and self.journal_id not in self.available_invoicing_journal_ids
+        ):
+            self.journal_id = False
+
     def _prepare_invoice(self):
         self.ensure_one()
         self._check_allowed_invoicing_journal(self.journal_id)
         return super()._prepare_invoice()
-
